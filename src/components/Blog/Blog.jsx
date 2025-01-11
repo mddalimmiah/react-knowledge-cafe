036e6f0c57584b83759f537ef7a7764a461060dc
mddalimmiah <mddalimsaif@gmail.com>
2025-01-12 01:48:43 +0600

    added blog destructuring data

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,30 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-    console.log(blog)
+    // console.log(blog)
+    const {title, cover, author, posted_date, author_img, reading_time, hashtags } =blog;
+
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h2 className="text-2xl">{author}</h2>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx} className='mx-3'><a href="">#{hash}</a></span>)
+                }
+            </p>
+
         </div>
     );
 };
